0004447f61051a0f49cb32e6d2e8f40688fcb1dd
jeusgao <jeusgao@163.com>
2021-03-15 16:38:52 +0800

    update rest_servce

--- rest_server.py
+++ rest_server.py
@@ -9,7 +9,7 @@ import argparse
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="args of rest service")
-    parser.add_argument("--host", default='127.0.0.1')
+    parser.add_argument("--host", default='0.0.0.0')
     parser.add_argument("-p", "--port", type=int, default=9055)
     parser.add_argument("-r", "--reload", type=int, default=1)
     args = parser.parse_args()
